52a64dfa08a25dc2f852603296412fc66d09f51a
remi <rnsandjaja@gmail.com>
2025-03-31 17:59:19 +0700

    added choices for the options

--- src/register-commands.js
+++ src/register-commands.js
@@ -19,12 +19,40 @@ const commands = [
                 name: 'first-number',
                 description: 'The first number',
                 type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: '1',
+                    },
+                    {
+                        name: 'two',
+                        value: '2',
+                    },
+                    {
+                        name: 'three',
+                        value: '3',
+                    },
+                ],
                 required: true,
             },
             {
                 name: 'second-number',
                 description: 'The second number',
                 type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: '1',
+                    },
+                    {
+                        name: 'two',
+                        value: '2',
+                    },
+                    {
+                        name: 'three',
+                        value: '3',
+                    },
+                ],
                 required: true,
             },
         ]
